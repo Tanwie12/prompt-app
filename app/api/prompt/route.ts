33303326238b86cf70dbf9ee658a1da24aa7f897
TANWIE12 <adeytanwie@gmail.com>
2024-05-10 21:59:09 +0100

    trying to fix feed getting all prompt bug

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,21 +1,19 @@
 import { connectToDatabase } from "@utils/database";
 import Prompt from "@models/prompt";
-import { NextResponse,NextRequest } from "next/server";
 
-export const GET=async(req:Request, {params}:{params:{id:string}}) => {
+
+export const GET=async(request:any) => {
   try {
     console.log('this users all prompt')
     await connectToDatabase();
     const prompts = await Prompt.find({}).populate('creator')
    
     return new Response(JSON.stringify(prompts), {
-      headers: {
-        'Content-Type': 'application/json',
-      },
+     
       status: 200,
     });
   } catch (error) {
-    console.error(error);
+   
    return new Response(JSON.stringify({ error: 'Something went wrong' }), {
       status: 500
     });
